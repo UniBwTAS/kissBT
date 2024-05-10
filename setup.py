d612c25e93fa8ab69894997f05dcb05d0475c53e
Tommy Steinecker <steineckertommy@gmail.com>
2024-05-10 13:26:48 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='kissBT',
     version="1.0.0",
-    packages=["kissBT"],
+    # packages=["kissBT"],
+    packages=find_packages(),
     url='https://github.com/UniBwTAS/kissBT',
     author='Thomas Steinecker',
     author_email='thomas.steinecker@unibw.de',
